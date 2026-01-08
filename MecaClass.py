1fe7b224b469ffbf14682141a9c9b7bf23eee222
RoieEzraty <roie.ezraty@gmail.com>
2026-01-08 13:01:22 +0200

    added tolerance in z

--- MecaClass.py
+++ MecaClass.py
@@ -34,6 +34,7 @@ class MecaClass:
         self.x_origin = self.cfg.getfloat("position.origin", "x_origin")
         self.y_origin = self.cfg.getfloat("position.origin", "y_origin")
         self.z_origin = self.cfg.getfloat("position.origin", "z_origin")
+        self.z_sleep = self.cfg.getfloat("position.origin", "z_sleep")
         self.theta_x_origin = self.cfg.getfloat("position.origin", "theta_x_origin")
         self.theta_y_origin = self.cfg.getfloat("position.origin", "theta_y_origin")
         self.theta_z_origin = self.cfg.getfloat("position.origin", "theta_z_origin")
@@ -69,6 +70,9 @@ class MecaClass:
         robot_helpers.apply_motion_config(self.robot, self.cfg)
         logger.info("Config parameters done")
 
+        # log (and display) current position
+        logger.info("Current arm position: %s", tuple(self.robot.GetPose()))
+
     def home(self):
         logger.info("Homing")
         # robot_helpers.assert_ready(self.robot)
@@ -76,10 +80,22 @@ class MecaClass:
         self.robot.WaitHomed()
         logger.info("Robot at home")
 
+    def set_TRF_wrt_holder(self):
+        load_cell_thick = self.cfg.getfloat("position.origin", "load_cell_thick", fallback=None)
+        holder_len = self.cfg.getfloat("position.origin", "holder_len", fallback=None)
+        self.tip_length = load_cell_thick + holder_len
+        self.robot.SetTrf(0.0, 0.0, self.tip_length, 0.0, 0.0, 0.0)
+
+    # def move_to_floor(self):
+    #     logger.info('Moving the robot to floor')
+
     def move_to_origin(self):
         logger.info('Moving the robot to origin')
-        self.move_lin(self, (self.x_origin, self.y_origin, self.z_origin,
-                             self.theta_x_origin, self.theta_y_origin, self.theta_z_origin))
+        self.MoveLin(self.x_origin/2, self.y_origin, self.z_sleep,
+                     self.theta_x_origin, self.theta_y_origin, self.theta_z_origin)
+        self.robot.WaitIdle()
+        self.MoveLin(self.x_origin, self.y_origin, self.z_origin,
+                     self.theta_x_origin, self.theta_y_origin, self.theta_z_origin)
 
     def move_to_sleep_pos(self):
         logger.info('Moving the robot to sleep position')
